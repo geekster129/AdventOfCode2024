fe4956b8cfc0d69b97e10d180eda0d0d94be767d
Test <test@gmail.com>
2024-12-01 21:36:58 +0800

    Preparation for day 2

---
+++ Day2/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] lines = File.ReadAllLines("D:\\AdventOfCode2024\\Day2\\puzzle2.txt");
+
+Part1(lines);
+Part2(lines);
+
+void Part1(string[] lines)
+{
+    Console.WriteLine("Part 1");
+}
+
+void Part2(string[] lines)
+{
+    Console.WriteLine("Part 2");
+}
