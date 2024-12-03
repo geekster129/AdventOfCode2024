e79a3b383073e7f3e190e4972c7d680651d076ad
Test <test@gmail.com>
2024-12-03 22:25:19 +0800

    Use regex groups for simplicity

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,21 +1,22 @@
 ﻿using System.Text.RegularExpressions;
 using System.Text;
 
-string input = File.ReadAllText("puzzle3.txt");
+string input = File.ReadAllText("d:\\AdventOfCode2024\\Day3\\puzzle3.txt");
 
 Console.WriteLine($"p1: {Part1(input)}");
 Console.WriteLine($"p2: {Part2(input)}");
 
 int Part1(string input) {
     int sum = 0;
-    var pattern = @"(mul[(]\d+,\d+[)])";
-    try {
+    var pattern = @"mul\((\d+),(\d+)\)";
+    try 
+    {
          foreach (Match match in Regex.Matches(input, pattern,
                                                RegexOptions.None,
-                                               TimeSpan.FromSeconds(1))) {                                                
-                                                    var val1 = Convert.ToInt32(match.Value.Split(",")[0].Split("mul(")[1]);
-                                                    var val2 = Convert.ToInt32(match.Value.Split(",")[1].Split(")")[0]);
-                                                    sum += val1*val2;
+                                               TimeSpan.FromSeconds(1))) {
+            var val1 = Convert.ToInt32(match.Groups[1].Value);
+            var val2 = Convert.ToInt32(match.Groups[2].Value);
+            sum += val1*val2;
                                                }
     }
     catch (RegexMatchTimeoutException) {
