77fbb64c6c94815939809e562841fad3420c2927
Test <test@gmail.com>
2024-12-03 08:12:15 +0800

    Template preparation for day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] lines = File.ReadAllLines("/workspaces/AdventOfCode2024/Day3/puzzle3.txt");
+
+Part1(lines);
+Part2(lines);
+
+void Part1(string[] lines)
+{
+    var p1 = 0;
+    foreach(var line in lines) 
+    {
+        var elems = line.Split();
+    }
+    Console.WriteLine($"p1: {p1}");
+}
+
+void Part2(string[] lines)
+{
+    var p2 = 0;
+    foreach (var line in lines)
+    {
+        var elems = line.Split();
+    }
+    Console.WriteLine($"p2: {p2}");
+}
