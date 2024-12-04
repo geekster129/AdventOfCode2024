dae23a9aaf26fbafcf9d6e198ae623a1453866d8
geekster129 <munyau@gmail.com>
2024-12-04 03:05:11 +0000

    Add array flavors

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,14 +1,17 @@
-﻿string input = File.ReadAllText("puzzle4.txt");
+﻿using System;
+
+string input = File.ReadAllText("puzzle4.txt");
+string[] input = File.ReadAllLines("puzzle4.txt");
 
 Console.WriteLine($"p1: {Part1(input)}");
 Console.WriteLine($"p2: {Part2(input)}");
 
-int Part1(string input)
+int Part1(string[] input)
 {
     return 0;
 }
 
-int Part2(string input)
+int Part2(string[] input)
 {
    return 0;
 }
