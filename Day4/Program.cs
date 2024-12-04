5e96524b3463ea489f6cff86f93e2b33b9d7ef19
geekster129 <munyau@gmail.com>
2024-12-04 02:49:28 +0000

    Day 4 prep

---
+++ Day4/Program.cs
@@ -0,0 +1,14 @@
+﻿string input = File.ReadAllText("puzzle4.txt");
+
+Console.WriteLine($"p1: {Part1(input)}");
+Console.WriteLine($"p2: {Part2(input)}");
+
+int Part1(string input)
+{
+    return 0;
+}
+
+int Part2(string input)
+{
+   return 0;
+}
