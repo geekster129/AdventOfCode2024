9798df144e9aa2517d1e680c1a4715027185d516
geekster129 <munyau@gmail.com>
2024-12-04 06:06:38 +0000

    Save the work

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
-string input = File.ReadAllText("puzzle4.txt");
-string[] input = File.ReadAllLines("puzzle4.txt");
+//string input = File.ReadAllText("puzzle4.txt");
+string[] input = File.ReadAllLines("test.txt");
 
 Console.WriteLine($"p1: {Part1(input)}");
 Console.WriteLine($"p2: {Part2(input)}");
 
 int Part1(string[] input)
 {
-    return 0;
+    var sum = 0;
+    foreach(var line in input) 
+    {
+        var xmasCnt = Regex.Matches(line, "XMAS", RegexOptions.None, TimeSpan.FromSeconds(1)).Count;
+        var samxCnt = Regex.Matches(line, "SAMX", RegexOptions.None, TimeSpan.FromSeconds(1)).Count;
+        Console.WriteLine($"{line} {xmasCnt} {samxCnt}");
+        sum += xmasCnt + samxCnt;
+    }
+
+    return sum;
 }
 
 int Part2(string[] input)
