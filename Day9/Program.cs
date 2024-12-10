0313783d220101bc8e124bc46d159a610a15ffdf
geekster129 <munyau@gmail.com>
2024-12-10 05:53:23 +0000

    Day 9 solution covering edge cases

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -7,44 +7,73 @@ string input = File.ReadAllText("puzzle9.txt");
 Console.WriteLine($"p1: {Part1(input)}");
 Console.WriteLine($"p2: {Part2(input)}");
 
-// test - 2333133121414131402 to 00...111...2...333.44.5555.6666.777.888899
-int Part1(string input)
+List<string> repeatedString(string str, int times) 
 {
-    var checksum = 0;
-    StringBuilder block = new StringBuilder();
+    return Enumerable.Repeat(str, times).ToList();
+}
+
+int findFreeSpace(List<string> str) {
+    for(int x = 0;x <str.Count; x++)
+    {    
+        if(str[x]==".") {  
+            return x;
+        }
+    }
+    return 0;
+}
+
+
+List<string> TrimEnd(List<string> lst) 
+{
+    
+    int last = 0;
+    for(int x = lst.Count-1; x>0; x--) 
+    {
+        if(lst[x]!=".") 
+        {
+            last = x+1;
+            break;
+        }        
+    }    
+    return lst.Take(last).ToList();
+
+}
+
+// test - 2333133121414131402 to 00...111...2...333.44.5555.6666.777.888899
+long Part1(string input)
+{
+    long checksum = 0;
+    List<string> block = [];
     for(int i = 0;i<=input.Length/2; i++ ) 
     {
         int usage = Convert.ToInt32(input[(i*2)].ToString());        
-        block.Append(new string(i.ToString().ToCharArray()[0], usage));
+        block.AddRange(repeatedString(i.ToString(), usage));
 
         if((i*2)+1 < input.Length)
         {
             int free = Convert.ToInt32(input[(i*2)+1].ToString());
-            block.Append(new string('.', free));
+            block.AddRange(repeatedString(".", free));
         }
     }
 
-    var defragged = "";
-    var finalBlock = block.ToString().ToCharArray();
-    for(int k = finalBlock.Length - 1; k > 0; k--) 
+    List<string> defragged = [];
+    for(int k = block.Count - 1; k > 0; k--) 
     {
         var lastNum = block[k];
-        var firstFreePos = new string(finalBlock).Split('.')[0].Length;        
-        finalBlock[k] = '.';
-        finalBlock[firstFreePos]=lastNum;
-        defragged = new string(finalBlock).TrimEnd('.');
-        if(!defragged.Contains('.'))
+        var firstFreePos = findFreeSpace(block);             
+        block[k] = ".";
+        block[firstFreePos]=lastNum;
+        defragged = TrimEnd(block);
+        if(!defragged.Any(d => d == "."))
             break; 
     }
 
-    Console.WriteLine(defragged);
-    var xDefrag = defragged.ToCharArray().Select(x => x).ToList();
-    for(int x = 0; x < xDefrag.Count; x++) 
+    for(int x = 0; x < defragged.Count; x++) 
     {
-        var num = Convert.ToInt32(xDefrag[x].ToString());
+        var num = Convert.ToInt32(defragged[x]);
         checksum += x * num;
     }
-
+    
     return checksum;
 }
 
