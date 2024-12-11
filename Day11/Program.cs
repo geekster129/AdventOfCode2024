623470ac5739cde421a77b4afee210ab47ac2a95
Test <test@gmail.com>
2024-12-12 07:55:15 +0800

    Day 11 solutions

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -1,52 +1,53 @@
-﻿using System;
-using System.Linq;
-using System.Text;
-
-string input = File.ReadAllText("test.txt");
+﻿string input = File.ReadAllText("puzzle11.txt");
 
 Console.WriteLine($"p1: {Part1(input)}");
 Console.WriteLine($"p2: {Part2(input)}");
 
-int GetNewStones(string input, int blinkTimes) {
-    var stones = input.Split().Select(x => Convert.ToInt64(x)).ToList();
-    List<long> changedStones = [];
+long GetNewStones(string input, int blinkTimes) {
+    Dictionary<long, long> aggregateMap = input.Split().GroupBy(x => Convert.ToInt64(x)).ToDictionary(x => x.Key, x => 1L);
     for(int i = 0; i < blinkTimes; i++) {
-        foreach(var stone in stones.ToList())
+        foreach(var stone in aggregateMap.ToList())
         {
-            var stoneStr = stone.ToString();
+            aggregateMap[stone.Key] -= stone.Value;
+
+            var stoneStr = stone.Key.ToString();
             if(stoneStr == "0") 
             {
-                changedStones.Add(1);
+                aggregateMap.TryAdd(1, 0L);
+                aggregateMap[1] += stone.Value;
             }
             else if(stoneStr.Length%2 == 0) 
             {            
                 var mid = stoneStr.Length/2;
-                var left = stoneStr.Substring(0, mid);
-                var right = stoneStr.Substring(mid, mid);
-                changedStones.Add(Convert.ToInt64(left));
-                changedStones.Add(Convert.ToInt64(right));
+                var left = Convert.ToInt64(stoneStr[..mid]);
+                var right = Convert.ToInt64(stoneStr[mid..]);
+
+                aggregateMap.TryAdd(left, 0L);
+                aggregateMap.TryAdd(right, 0L);
+
+                aggregateMap[left] += stone.Value;
+                aggregateMap[right] += stone.Value;
             }
             else
             {
-                changedStones.Add(Convert.ToInt64(stone)*2024);
-            }        
+                aggregateMap.TryAdd(Convert.ToInt64(stoneStr) * 2024, 0L);
+                aggregateMap[Convert.ToInt64(stoneStr) * 2024] += stone.Value;    
+            }
+
         }
-        //Console.WriteLine(string.Join(" ", changedStones));
+        //Console.WriteLine(string.Join(" ", aggregateMap.Keys));
         
 
-        stones = changedStones;
-        Console.WriteLine($"{i}: {stones.Count}");
-        changedStones = [];
     }
-    return stones.Count;
+    return aggregateMap.Values.Sum();
 }
 
-int Part1(string input)
+long Part1(string input)
 {
     return GetNewStones(input, 25);
 }
 
-int Part2(string input)
+long Part2(string input)
 {
    return GetNewStones(input, 75);
 }
